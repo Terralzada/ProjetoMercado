f51f67fa02dc68fd15306df326e31c03892c2403
G1ss <Giovanio9@outlook.com>
2023-06-14 21:26:06 -0300

    Construtor + criacao de mapa

--- Labirinto.java
+++ Labirinto.java
@@ -1,2 +1,36 @@
+import java.util.Random;
 public class Labirinto {
+
+    //Atributos
+    private int parede , entrada, saida;
+    private int x, y, nParedes, xFulano, yFulano;
+    private int mapa[][];
+    private Random rand;
+
+    //construtor
+    public Labirinto(){
+        parede = 1;
+        entrada = 3;
+        saida = 5;
+        rand = new Random();
+        x = 0;
+        y = 0;
+        xFulano = 0;
+        yFulano = 0;
+        nParedes = 0;
+        criarMapa();
+    }
+
+    private void criarMapa(){
+        //definicao de caminho
+        gerarCaminho();
+
+        //definicao da saida
+        randomizar();
+        mapa[x][y] = saida;
+
+        //definicao da entrada
+        while(mapa[x][y] == saida){
+            randomizar();
+        }
 }
