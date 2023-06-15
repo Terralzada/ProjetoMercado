0b097691673faf91c61928c82f63bb33f8c09e9a
Amanda Velloso <32628647+vellsant@users.noreply.github.com>
2023-06-14 21:40:00 -0300

    Main AppRato

--- AppRato.java
+++ AppRato.java
@@ -2,5 +2,23 @@ public class AppRato {
 
     public static void main(String[] args) {
 
+        Labirinto teste = new Labirinto();
+        Rato mickey = new Rato(teste);
+
+        System.out.println("Mapa Criado:");
+        teste.mostrarMapa();
+        System.out.println();
+        System.out.println();
+
+
+        if(mickey.acharSaida('j', teste.getxRato(), teste.getyRato())){
+            System.out.println("Achou a saida");
+        }else{
+            System.out.println("Nao achou a saida");
+        }
+
+        System.out.println();
+        System.out.println("Caminho percorrido:");
+        ciclano.mostrarCaminho();
     }
 }
