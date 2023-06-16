50f2ae530f9ec98a5aaff7bcaff85a9966a52a97
G1ss <Giovanio9@outlook.com>
2023-06-15 21:14:42 -0300

    ajuste_teste_2

--- Tests/Teste.java
+++ Tests/Teste.java
@@ -2,8 +2,6 @@ package Tests;
 
 import Source.*;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
